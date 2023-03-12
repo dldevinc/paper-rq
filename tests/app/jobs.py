336ed817b31b64f961b72c3b45f97cfd286ce334
pix666 <x896321475@gmail.com>
2023-03-12 11:47:40 +0400

    add unstable job example

--- tests/app/jobs.py
+++ tests/app/jobs.py
@@ -1,6 +1,14 @@
 import time
+import random
 
 
 def sleep(delay):
     time.sleep(delay)
+    return delay
+
+
+def unstable_sleep(delay):
+    time.sleep(delay)
+    if random.randint(0, 3) == 2:
+        raise RuntimeError("Bad luck...")
     return delay
