6d2c7f44e3e05590739d0235672a26da6281df5e
pix666 <x896321475@gmail.com>
2023-03-12 11:47:20 +0400

    add format settings

--- tests/settings.py
+++ tests/settings.py
@@ -104,6 +104,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+FORMAT_MODULE_PATH = [
+    "tests.formats",
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
